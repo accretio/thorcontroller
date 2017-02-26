fd668de03c6e259e784524f90e3a80b92a992e9e
William Le Ferrand <william.le-ferrand@polytechnique.edu>
2017-02-26 12:01:57 -0800

    initial startup event to statsd

--- statsd_octoprint_plugin/__init__.py
+++ statsd_octoprint_plugin/__init__.py
@@ -15,10 +15,10 @@ class StatsdPlugin(octoprint.plugin.StartupPlugin):
         self.interval = 30.0
         
         # send a first event
-        statsd.increment("station.restarts")
+        statsd.event('Station start', 'Octoprint started')
        
     def on_after_startup(self):
-        self._logger.info("Hello World!") 
+        self._logger.info("Starting the stats plugin") 
         self.startTimer()
                 
     def startTimer(self):
@@ -27,12 +27,14 @@ class StatsdPlugin(octoprint.plugin.StartupPlugin):
 
     def collectMetrics(self):
         self._logger.info("Collecting metrics")
-
         if sys.platform == "linux2":
             with file("/sys/class/thermal/thermal_zone0/temp") as f:
                 s = f.read()
-                statsd.gauge('temperature.cpu', float(temp) / 100.0)
-                
+                try:
+                    statsd.gauge('system.cpu.temperature', float(s) / 1000.0)
+                except:
+                    self._logger.error("Unexpected error: %s" % (sys.exc_info()[0]))
+                                    
     
 __plugin_name__ = "Statsd plugin"
 __plugin_version__ = "1.0.0"
