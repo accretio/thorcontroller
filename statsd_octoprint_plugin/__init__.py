a3b927b401d3c576b2af75fc899d6f2cdabef691
William Le Ferrand <william.le-ferrand@polytechnique.edu>
2017-02-26 15:58:54 -0800

    adding printer temperatures

--- statsd_octoprint_plugin/__init__.py
+++ statsd_octoprint_plugin/__init__.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 # inspired from https://github.com/imrahil/OctoPrint-NavbarTemp/blob/master/octoprint_navbartemp/__init__.py
 
 import octoprint.plugin
+import octoprint.printer
 from octoprint.util import RepeatedTimer
 import sys
 import re
@@ -25,13 +26,26 @@ class StatsdPlugin(octoprint.plugin.StartupPlugin):
         self._checkTempTimer = RepeatedTimer(self.interval, self.collectMetrics, None, None, True)
         self._checkTempTimer.start()
 
+    def collectPrinterTemperatures(self):
+        try:
+            temps = self._printer.get_current_temperatures()
+            if len(temps) > 0:
+                self._logger.info("temps are %s" % temps)
+                statsd.gauge('printer.temperatures.bed.actual', temps['bed']['actual'])
+                statsd.gauge('printer.temperatures.bed.target', temps['bed']['target'])
+                statsd.gauge('printer.temperatures.extruder.actual', temps['extruder']['actual'])
+                statsd.gauge('printer.temperatures.extruder.target', temps['extruder']['target'])
+        except:
+            self._logger.error("Unexpected error: %s" % (sys.exc_info()[0]))
+
     def collectMetrics(self):
         self._logger.info("Collecting metrics")
+        self.collectPrinterTemperatures()
         if sys.platform == "linux2":
             with file("/sys/class/thermal/thermal_zone0/temp") as f:
                 s = f.read()
                 try:
-                    statsd.gauge('system.cpu.temperature', float(s) / 1000.0)
+                    statsd.gauge('system.cpu.temperature', ((float(s) / 1000.0 - 32) * 5 / 9))
                 except:
                     self._logger.error("Unexpected error: %s" % (sys.exc_info()[0]))
                                     
